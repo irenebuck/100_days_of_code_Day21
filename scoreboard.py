b048a83104dd49f004182eaf8442d20b49b71a06
Irene Buck <irenedevito@gmail.com>
2023-08-31 19:37:06 -0700

    Updated game to save high score using a text file

--- scoreboard.py
+++ scoreboard.py
@@ -6,6 +6,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open('data.txt') as data:
+            self.highest_score = int(data.read())
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -15,11 +17,14 @@ class Scoreboard(Turtle):
     def update_score(self):
         self.score += 1
         self.clear()
-        self.write(f'Score: {self.score}', align="center", font=("Arial", 12, "bold"))
+        self.write(f'Score: {self.score} Highest Score: {self.highest_score}', align="center", font=("Arial", 12, "bold"))
 
-    def game_over(self):
-        self.penup()
-        self.goto(0, 0)
-        self.write(f'GAME OVER', align="center", font=("Arial", 25, "bold"))
-
-
+    def reset(self):
+        if self.score > self.highest_score:
+            self.highest_score = self.score
+            with open('data.txt', mode='w') as data:
+                data.write(f'{self.score}')
+        self.score = 0
+        self.clear()
+        self.write(f'Score: {self.score} Highest Score: {self.highest_score}', align="center",
+                   font=("Arial", 12, "bold"))
